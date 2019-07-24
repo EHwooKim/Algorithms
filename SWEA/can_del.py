d7c0d3151637d491de98382fcaa549ff9c7a6ca7
EHwooKim <ehwoo0707@naver.com>
2019-07-24 17:22:16 +0900

    190724| SWEA D2

--- SWEA/can_del.py
+++ SWEA/can_del.py
@@ -1,14 +1,2 @@
-test = int(input())
-
-
-line_count = int(input()):
-
-for i in range(line_count):
-    line = []
-    
-
-
-
-line =[]
-alphabet = input()
-count = int(input())
+x={3:2, 3:4}
+print(x)
