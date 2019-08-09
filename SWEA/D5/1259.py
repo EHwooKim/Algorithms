cb26fb6cf2c5770781bb17a6cbb01993369316de
EHwooKim <ehwoo0707@naver.com>
2019-08-09 09:01:50 +0900

    190809 | #1259 수정

--- SWEA/D5/1259.py
+++ SWEA/D5/1259.py
@@ -4,14 +4,9 @@ T = int(input())
 for t in range(1, T + 1):
     n = int(input())
     n_list = list(map(int, input().split()))   # 입력
-    x = []
-    y = []
-    for i in range(2*n):
-        if i % 2:
-            y.append(n_list[i])
-        else:
-            x.append(n_list[i])
-    arr = list(zip(x, y))                        # 두개씩 나누기
+    arr = []
+    for i in range(n):
+        arr.append(n_list[2*i:2*i+2])         # 두개씩 나누기           
 
     for one in arr:
         result = []
@@ -23,6 +18,4 @@ for t in range(1, T + 1):
         if len(result) == 2 * n:
             break
 
-    print('#{}'.format(t), end=' ')  # 출력
-    print(*result)
-
+    print('#{} '.format(t), *result)  # 출력
