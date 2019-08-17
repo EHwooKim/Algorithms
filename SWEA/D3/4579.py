b442e6facf1d229b69229744b1fadbd7206225ec
EHwooKim <ehwoo0707@naver.com>
2019-08-17 09:02:21 +0900

    190817 | 4579.py 수정

--- SWEA/D3/4579.py
+++ SWEA/D3/4579.py
@@ -1,9 +1,9 @@
 def palindrome(word):
     for i in range(len(word) // 2):
-        if word[i] != word[-1-i] and word[i] != '*' and word[-1-i] != '*':
+        if word[i] == '*' or word[-1-i] == '*':
+            return 'Exist' 
+        elif word[i] != word[-1-i]:
             return 'Not exist'
-        elif word[i] == '*' or word[-1-i] == '*':
-            return 'Exist'
     return 'Exist'
 
 T = int(input())
