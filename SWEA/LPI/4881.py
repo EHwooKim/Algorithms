54e70a0a016a85dbb305b05c248e7cf3aeed7a4c
EHwooKim <ehwoo0707@naver.com>
2019-08-29 11:30:47 +0900

    190829 | SWEA #4881 - recur.permutation

--- SWEA/LPI/4881.py
+++ SWEA/LPI/4881.py
@@ -1,14 +1,22 @@
-# 아직 풀지 못했습니다.
+def perm(k, n, used, cur_sum): 
+    global MIN
+    if cur_sum >= MIN: return
+    if k == n:
+        MIN = min(MIN, cur_sum)
+        return
+
+    for i in range(N):
+        if used & (1 << i): continue  
+        order[k] = i
+        perm(k + 1, n, used | (1 <<i), cur_sum + N_list[k][order[k]]) 
 T = int(input())
 
 for t in range(1, T + 1):
     N = int(input())
     N_list =[list(map(int, input().split())) for _ in range(N)]
+    order = [0] * N 
+    MIN = 0xffffff
 
 
-
-
-
-
-
-
+    perm(0, N, 0, 0)  
+    print('#{} {}'.format(t, MIN))
