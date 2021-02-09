5465b3c996c641b4b3e3202dcd2653283e1b772e
EHwooKim <ehwoo0707@naver.com>
2021-02-09 22:39:22 +0900

    Update code 9498

--- 백준/code/9498.js
+++ 백준/code/9498.js
@@ -4,10 +4,10 @@ console.log(solution(input))
 
 function solution(number) {
   let grade = 'F'
-  if (number >= 90 & number <= 100) grade = 'A'
-  else if (number >= 80 & number <= 89) grade = 'B'
-  else if (number >= 70 & number <= 79) grade = 'C'
-  else if (number >= 60 & number <= 69) grade = 'D'
+  if (number >= 90) grade = 'A'
+  else if (number >= 80) grade = 'B'
+  else if (number >= 70) grade = 'C'
+  else if (number >= 60) grade = 'D'
 
   return grade
 }
