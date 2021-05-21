67673c0899cfb9d94626e4682a56f47c05d0050d
EHwooKim <ehwoo0707@naver.com>
2021-05-21 16:10:31 +0900

    Update BOJ - 10039

--- 백준/code/10039.js
+++ 백준/code/10039.js
@@ -3,40 +3,53 @@ const rl = require('readline').createInterface({
   output: process.stdout
 })
 
-// const curry = f => 
-//   (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._)
-// const reduce = curry((f, acc, iter) => {
-//   if (!iter) {
-//     iter = acc[Symbol.iterator]()
-//     acc = iter.next().value
-//   }
-//   for (const a of iter) {
-//     acc = f(acc, a)
-//   }
-//   return acc
-// })
-// const go = (...args) => reduce((a, f) => f(a), args)
-// const L = {}
-// L.map = curry(function *(f, iter) {
-//   for (const a of iter) yield f(a)
-// })
-
-// const sum = (a, b) => a + b
+const curry = f => 
+  (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._)
+const reduce = curry((f, acc, iter) => {
+  if (!iter) {
+    iter = acc[Symbol.iterator]()
+    acc = iter.next().value
+  }
+  for (const a of iter) {
+    acc = f(acc, a)
+  }
+  return acc
+})
+const go = (...args) => reduce((a, f) => f(a), args)
+const L = {}
+L.map = curry(function *(f, iter) {
+  for (const a of iter) {
+    yield f(a)
+  }
+})
 
 const input = []
 rl.on('line', line => {
   input.push(+line)
 }).on('close', () => {
 
-  console.log(solution(input))
+  const setMinValue = min => value => Math.max(min, value)
+  
+  const average = curry((length, iter) => reduce((a, b) => a + b/length, 0, iter))
 
-  function solution(arr) {
-    const total = arr.reduce((a, b) => {
-      if (b < 40) b = 40
-      return a + b
-    }, 0)
-    return total / arr.length
+  console.log(solution(input, 40))
+
+  function solution(input, min) {
+    return go(
+      input,
+      L.map(setMinValue(min)),
+      average(input.length)
+    )
   }
 
   process.exit()
 })
+
+
+// function solution(arr) {
+//   const total = arr.reduce((a, b) => {
+//     if (b < 40) b = 40
+//     return a + b
+//   }, 0)
+//   return total / arr.length
+// }
